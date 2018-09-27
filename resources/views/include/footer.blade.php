30bfd24fcfa3f601bfb7d8c95e477dd8e5fbd4b9
sharbel chris <sharbel.cosa@gmail.com>
2018-09-27 11:28:16 +0300

    adding footer and sections of home page

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,7 +1,65 @@
+<!-- =========================== FOOTER BEGIN  =========================== -->
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-3">
+       <h3>Location</h3>
+       <p>House of Grace Eldoret is located in Kapsoya, opposite The Noble Hotel, a few meters from the Nairobi – Eldoret Highway. </p>
+    </div>
+    <div class="col-md-3">
+       <h3>Our Vision</h3>
+        <p>To transform the lives of its members and subsequently
+            the society at large to be like Christ by teaching and training them
+            to be witnesses of our Lord Jesus Christ, 1 Cor15:10,
+            “but by the grace of God I am who I am.”
+        </p>
+    </div>
+    <div class="col-md-3">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Our Mission</h3>
+                <p>
+                    To equip and prepare our members to be
+                    witnesses for our Lord Jesus Christ by
+                    their experiences of His grace.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+<h3>Ministry Motto</h3>
+                <p>
+                    Reaching the Lost Through our witness.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Ministry Slogan</h3>
+                <p>Doing church as a team.</p>
+            </div>
+        </div>
 
-<p style="background-color: #0000F0">my footer</p>
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Social media icons</h3>
+                <a href="#"><i class="fab fa-facebook-square fa-2x"></i></a>
+                <a href="#"><i class="fab fa-twitter-square fa-2x"></i></a>
+                <a href="#"><i class="fab fa-instagram fa-2x"></i></a>
+            </div>
+        </div>
+
     </div>
 </div>
+<!-- =========================== FOOTER END =========================== -->
+<!-- =========================== COPYRIGHT START =========================== -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            &copy; Copyright 2018 | House Of Grace Church Eldoret | All Rights Reserved
+        </div>
+    </div>
+</div>
+<!-- =========================== COPYRIGHT END =========================== -->
+
 
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 @section('content')
+    <!-- =========================== SLIDER BEGIN  =========================== -->
     <div class="owl-carousel owl-theme">
         <!-- first slide -->
         <div class="item " >
             <img src="{{asset('images/d.jpg')}}">
             <div class="bottom-left">
 
-            <h4 class data-animation-in="fadeInLeft" data-animation-out="animate-out fadeOutRight">Slide 1</h4>
-            <h6 data-animation-in="fadeInRight" data-animation-out="animate-out fadeOutLeft">
-                Cras a elementum dolor. Praesent aliquam sapien ac eros semper ullamcorper. Sed imperdiet enim at
-                sodales suscipit. Aenean eget faucibus ipsum.
-            </h6>
+                <h4  data-animation-in="fadeInLeft" data-animation-out="animate-out fadeOutRight">Slide 1</h4>
+                <h6 data-animation-in="fadeInRight" data-animation-out="animate-out fadeOutLeft">
+                    Cras a elementum dolor. Praesent aliquam sapien ac eros semper ullamcorper. Sed imperdiet enim at
+                    sodales suscipit. Aenean eget faucibus ipsum.
+                </h6>
 
             </div>
         </div>
@@ -20,10 +21,12 @@
             <img src="{{asset('images/d.jpg')}}">
 
             <div class="bottom-left">
-                <h4 class data-animation-in="fadeInLeft" data-animation-out="animate-out fadeOutRight">Slide 2</h4>
+                <h4  data-animation-in="fadeInLeft" data-animation-out="animate-out fadeOutRight">Slide 2</h4>
                 <h6 data-animation-in="fadeInRight" data-animation-out="animate-out fadeOutLeft">
-                Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit. Nunc id dolor pulvinar, mollis orci vitae, molestie elit. Maecenas scelerisque ipsum nibh, id imperdiet nulla lobortis nec.</h6>
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Nunc id dolor pulvinar, mollis orci vitae, molestie elit.
+                    Maecenas scelerisque ipsum nibh, id imperdiet nulla lobortis nec.
+                </h6>
 
             </div>
         </div>
@@ -32,15 +35,62 @@
         <div class="item">
             <img src="{{asset('images/d.jpg')}}">
             <div class="bottom-left">
-                <h4 class data-animation-in="fadeInLeft" data-animation-out="animate-out fadeOutRight">Slide 3</h4>
+                <h4 data-animation-in="fadeInLeft" data-animation-out="animate-out fadeOutRight">Slide 3</h4>
                 <h6 data-animation-in="fadeInRight" data-animation-out="animate-out fadeOutLeft">
-                Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit. Nunc id dolor pulvinar, mollis orci vitae, molestie elit. Maecenas
-                scelerisque ipsum nibh, id imperdiet nulla lobortis nec.<h6/>
-
+                    Lorem ipsum dolor sit
+                    amet, consectetur adipiscing elit. Nunc id dolor pulvinar, mollis orci vitae, molestie elit. Maecenas
+                    scelerisque ipsum nibh, id imperdiet nulla lobortis nec.
+                </h6>
             </div>
         </div>
     </div>
+    <!-- =========================== SLIDER END  =========================== -->
+
+
+    <!-- =========================== SERVICES BEGIN  =========================== -->
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Sunday services</h3>
+            <ul>
+                <li>Prayer Service : 7:00am - 8:00am</li>
+                <li>The Journey : 8:00am - 8:50am</li>
+                <li>Family Service : 9:00am - 12:00pm</li>
+                <li>Oasis of Grace Childrens Church : 9:00am - 12:00pm</li>
+
+            </ul>
+
+        </div>
+        <div class="col-md-6">
+            <h3>Other services</h3>
+            <ul>
+                <li>Blast Youth Service : Every fortnight 3:30pm - 6:00pm</li>
+                <li>Prayer BootCamp : Wednesdays 5:30pm - 7:30pm</li>
+                <li>Ladies Prayer Meeting: Tuesdays 5:00pm - 6:00pm</li>
+                <li>Throne Room Experience(TRE): 2nd Friday of the month 6:00pm - 8:00pm</li>
+            </ul>
+        </div>
+    </div>
+    <!-- =========================== SERVICES END  =========================== -->
+
+
+    <!-- =========================== UPCOMING EVENTS BEGIN  =========================== -->
+    <div class="row ">
+        <div class="col-md-12">
+            <div class="h3">Events</div>
+        </div>
+    </div>
+
+    <!-- =========================== UPCOMING EVENTS END  =========================== -->
+
+    <!-- =========================== LOCATION GOOGLE MAPS BEGIN  =========================== -->
+    <div class="row">
+        <div class="col-md-12">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15958.639785944462!2d35.297056!3d0
+            .51051!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x901065d104b128fe!2sHouse+Of+Grace!5e0!3m2!1sen!2ske
+            !4v1537967383843" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>
+    <!-- =========================== LOCATION GOOGLE MAPS END  =========================== -->
 @endsection
 @section('scripts')
     <script>
