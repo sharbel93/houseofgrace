805cd2cba4782d83641ed4638c6e459f0306ef6e
sharbel93 <sharbel@techfactory.ltd>
2019-11-11 23:37:07 +0300

    ui adjustment on dashboard title

--- resources/views/manage/dashboard.blade.php
+++ resources/views/manage/dashboard.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.manage')
 @section('title', '| Admin Home')
 @section('content')
- Dashboard
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h3>Dashboard</h3>
+            </div>
+        </div>
+    </div>
+
 
 @stop
--- resources/views/partials/admin/layouts/_footer.blade.php
+++ resources/views/partials/admin/layouts/_footer.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="footer-copy-right">
-                    <p>Copyright © 2018. All rights reserved. House of Grace Eldoret
+                    <p>Copyright © <script>document.write(new Date().getFullYear());</script>. All rights reserved. House of Grace Eldoret
                 </div>
             </div>
         </div>
